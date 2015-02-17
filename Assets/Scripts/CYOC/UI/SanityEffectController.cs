ae6b3c4fc70b7d85cabb48ebab10ae8be19e4458
arutledge <arutledge@mygogames.com>
2015-02-17 17:09:20 -0600

    begining of sanity

---
+++ Assets/Scripts/CYOC/UI/SanityEffectController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.ChoiceEngine;
+using Assets.Scripts.ICG.Messaging;
+using Assets.Scripts.ChoiceEngine.Messages;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.CYOC.UI
+{
+	public class SanityEffectController : MonoBehaviour
+	{
+		public PlayerStat PlayerStatistic;
+		public int TriggerValue;
+		
+		private void Awake()
+		{
+			MessageSystem.SubscribeMessage<PlayerStatChangedMessage>(MessageSystem.ServiceContext, OnStatChanged);
+		}
+		
+		private void Update()
+		{
+			
+		}
+		
+		private void OnDestroy()
+		{
+			MessageSystem.UnsubscribeMessage<PlayerStatChangedMessage>(MessageSystem.ServiceContext, OnStatChanged);
+		}
+		
+		private void OnStatChanged(PlayerStatChangedMessage message)
+		{
+			if (message.StatChanged == PlayerStatistic && message.NewValue > TriggerValue)
+			{
+			}
+			
+			if (message.StatChanged == PlayerStatistic && message.NewValue <= TriggerValue) 
+			{
+	
+			}
+		}	
+	}
+}
